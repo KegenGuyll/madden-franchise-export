2ff4e42d87fbeaf6ebd83d8a43dc63418bbde662
Kegen Guyll <guyllkegen@gmail.com>
2023-02-19 15:58:46 -0600

    removed firebase and prevent dups

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const {initializeApp} = require('firebase/app');
-const { getFirestore } = require('firebase/firestore')
 require("dotenv").config();
 
-const { MongoClient } = require('mongodb')
+const {
+  MongoClient
+} = require('mongodb')
 
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
@@ -31,197 +31,265 @@ const app = express();
 app.set('port', (process.env.PORT || 3001));
 
 app.get('*', (req, res) => {
-    res.send('Madden Companion Exporter');
+  res.send('Madden Companion Exporter');
 });
 
 // LEAGUE INFO
 app.post('/:platform/:leagueId/leagueteams', (req, res) => {
+  const {
+    params: {
+      leagueId
+    }
+  } = req;
+  const bulk = mongoService.db(leagueId).collection('teams').initializeUnorderedBulkOp()
+
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+  req.on('end', async () => {
     const {
-        params: { leagueId }
-    } = req;
-    const bulk = mongoService.db(leagueId).collection('teams').initializeUnorderedBulkOp()
+      leagueTeamInfoList: teams
+    } = JSON.parse(body);
 
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
+    teams.forEach(team => {
+      bulk.find({
+        teamId: team.teamId
+      }).upsert().replaceOne({
+        ...team
+      })
     });
-    req.on('end', async () => {
-        const { leagueTeamInfoList: teams } = JSON.parse(body);
 
-        teams.forEach(team => {
-            bulk.find({teamId: team.teamId}).upsert().replaceOne({...team})
-        });
+    await bulk.execute();
 
-        await bulk.execute();
-
-        res.sendStatus(200);
-    });
+    res.sendStatus(200);
+  });
 });
 
 // LEAGUE INFO
 app.post('/:platform/:leagueId/standings', (req, res) => {
+  const {
+    params: {
+      leagueId
+    }
+  } = req;
+
+  const bulk = mongoService.db(leagueId).collection('standings').initializeUnorderedBulkOp()
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+  req.on('end', async () => {
     const {
-        params: { leagueId }
-    } = req;
+      teamStandingInfoList: teams
+    } = JSON.parse(body);
 
-    const bulk = mongoService.db(leagueId).collection('standings').initializeUnorderedBulkOp()
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
+    teams.forEach(team => {
+      bulk.find({
+        teamId: team.teamId
+      }).upsert().replaceOne({
+        ...team
+      })
     });
-    req.on('end', async () => {
-        const { teamStandingInfoList: teams } = JSON.parse(body);
 
-        teams.forEach(team => {
-            bulk.find({teamId: team.teamId}).upsert().replaceOne({...team})
-        });
-
-        await bulk.execute();
-        res.sendStatus(200);
-    });
+    await bulk.execute();
+    res.sendStatus(200);
+  });
 });
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 app.post(
-    '/:platform/:leagueId/week/:weekType/:weekNumber/:dataType',
-    (req, res) => {
-        const {
-            params: { username, leagueId, weekType, weekNumber, dataType },
-        } = req;
-        const basePath = `data/${username}/${leagueId}/`;
-        // "defense", "kicking", "passing", "punting", "receiving", "rushing"
-        const statsPath = `${basePath}stats`;
+  '/:platform/:leagueId/week/:weekType/:weekNumber/:dataType',
+  (req, res) => {
+    const {
+      params: {
+        username,
+        leagueId,
+        weekType,
+        weekNumber,
+        dataType
+      },
+    } = req;
+    const basePath = `data/${username}/${leagueId}/`;
+    // "defense", "kicking", "passing", "punting", "receiving", "rushing"
+    const statsPath = `${basePath}stats`;
 
 
-        const bulkTeamStats = mongoService.db(leagueId).collection('teamstats').initializeUnorderedBulkOp()
-        const bulkDefenseStats = mongoService.db(leagueId).collection('defense').initializeUnorderedBulkOp()
-        const bulkPlayerStats = mongoService.db(leagueId).collection('playerstats').initializeUnorderedBulkOp()
+    const bulkTeamStats = mongoService.db(leagueId).collection('teamstats').initializeUnorderedBulkOp()
+    const bulkDefenseStats = mongoService.db(leagueId).collection('defense').initializeUnorderedBulkOp()
+    const bulkPlayerStats = mongoService.db(leagueId).collection('playerstats').initializeUnorderedBulkOp()
 
 
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', async () => {
-            switch (dataType) {
-                case 'schedules': {
-                    const { gameScheduleInfoList: schedules } = JSON.parse(body);
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', async () => {
+      switch (dataType) {
+        case 'schedules': {
+          const {
+            gameScheduleInfoList: schedules
+          } = JSON.parse(body);
 
-                    mongoService.db(leagueId).collection("schedules").insertMany(schedules)
-                    break;
-                }
-                case 'teamstats': {
-                    
-                    const { teamStatInfoList: teamStats } = JSON.parse(body);
-                    teamStats.forEach(stat => {
-                        bulkTeamStats.insert({...stat, weekType, weekNumber})
-                    });
+          mongoService.db(leagueId).collection("schedules").insertMany(schedules)
+          break;
+        }
+        case 'teamstats': {
 
-                    break;
-                }
-                case 'defense': {
-                    const { playerDefensiveStatInfoList: defensiveStats } = JSON.parse(body);
+          const {
+            teamStatInfoList: teamStats
+          } = JSON.parse(body);
+          teamStats.forEach(stat => {
+            bulkTeamStats
+            .find({statId: stat.statId})
+            .upsert()
+            .replaceOne({
+              ...stat,
+              weekType,
+              weekNumber
+            })
+          });
 
-                    defensiveStats.forEach(stat => {
-                        bulkDefenseStats.insert({...stat, weekType, weekNumber})
-                    });
-                    break;
-                }
-                default: {
-                    const property = `player${capitalizeFirstLetter(
-                        dataType
-                    )}StatInfoList`;
-                    const stats = JSON.parse(body)[property];
-                    
-                    stats.forEach(stat => {
-                        if(stat){
-                         bulkPlayerStats.insert({...stat, dataType, weekType, weekNumber})   
-                        }
-                        
-                    });
-                    break;
-                }
+          break;
+        }
+        case 'defense': {
+          const {
+            playerDefensiveStatInfoList: defensiveStats
+          } = JSON.parse(body);
+
+          defensiveStats.forEach(stat => {
+            bulkDefenseStats
+            .find({statId: stat.statId})
+            .upsert()
+            .replaceOne({
+              ...stat,
+              weekType,
+              weekNumber
+            })
+          });
+          break;
+        }
+        default: {
+          const property = `player${capitalizeFirstLetter(dataType)}StatInfoList`;
+          const stats = JSON.parse(body)[property];
+
+          stats.forEach(stat => {
+            if (stat) {
+              bulkPlayerStats
+              .find({statId: stat.statId})
+              .upsert()
+              .replaceOne({
+                ...stat,
+                dataType,
+                weekType,
+                weekNumber
+              })
             }
 
-            if(bulkDefenseStats.length > 0){
-                await bulkDefenseStats.execute()  
-            }
-            if(bulkPlayerStats.length > 0){
-                await bulkPlayerStats.execute()   
-            }
-            if(bulkTeamStats.length > 0) {
-                await bulkTeamStats.execute()   
-            }
+          });
+          break;
+        }
+      }
+
+      if (bulkDefenseStats.length > 0) {
+        await bulkDefenseStats.execute()
+      }
+      if (bulkPlayerStats.length > 0) {
+        await bulkPlayerStats.execute()
+      }
+      if (bulkTeamStats.length > 0) {
+        await bulkTeamStats.execute()
+      }
 
 
-            res.sendStatus(200);
-        });
-    }
+      res.sendStatus(200);
+    });
+  }
 );
 
 // ROSTERS
 app.post('/:platform/:leagueId/freeagents/roster', (req, res) => {
+  const {
+    params: {
+      username,
+      leagueId,
+      teamId
+    }
+  } = req;
+  let body = '';
+
+  const bulk = mongoService.db(leagueId).collection('players').initializeUnorderedBulkOp()
+
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+  req.on('end', async () => {
     const {
-        params: { username, leagueId, teamId }
-    } = req;
-    let body = '';
+      rosterInfoList
+    } = JSON.parse(body);
 
-    const bulk = mongoService.db(leagueId).collection('players').initializeUnorderedBulkOp()
+    console.log(JSON.parse(body))
 
-    req.on('data', chunk => {
-        body += chunk.toString();
+    if (!rosterInfoList) {
+      res.sendStatus('500')
+      return
+    }
+
+    rosterInfoList.forEach(player => {
+      bulk.find({
+        rosterId: player.rosterId
+      }).upsert().replaceOne({
+        ...player
+      })
     });
-    req.on('end', async () => {
-        const { rosterInfoList } = JSON.parse(body);
 
-        console.log(JSON.parse(body))
-
-        if(!rosterInfoList) {
-            res.sendStatus('500')
-            return
-        }
-
-        rosterInfoList.forEach(player => {
-            bulk.find({rosterId: player.rosterId}).upsert().replaceOne({...player})
-        });
-
-        await bulk.execute();
-        res.sendStatus(200);
-    });    
+    await bulk.execute();
+    res.sendStatus(200);
+  });
 });
 
 // ROSTER
 app.post('/:platform/:leagueId/team/:teamId/roster', async (req, res) => {
+  const {
+    params: {
+      username,
+      leagueId,
+      teamId
+    }
+  } = req;
+  let body = '';
+
+  const bulk = mongoService.db(leagueId).collection('players').initializeUnorderedBulkOp()
+
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+  req.on('end', async () => {
     const {
-        params: { username, leagueId, teamId }
-    } = req;
-    let body = '';
+      rosterInfoList
+    } = JSON.parse(body);
 
-    const bulk = mongoService.db(leagueId).collection('players').initializeUnorderedBulkOp()
+    if (!rosterInfoList) {
+      res.sendStatus('500')
+      return
+    }
 
-    req.on('data', chunk => {
-        body += chunk.toString();
+    rosterInfoList.forEach(player => {
+      bulk.find({
+        rosterId: player.rosterId
+      }).upsert().replaceOne({
+        ...player
+      })
     });
-    req.on('end', async () => {
-        const { rosterInfoList } = JSON.parse(body);
 
-        if(!rosterInfoList) {
-            res.sendStatus('500')
-            return
-        }
-
-        rosterInfoList.forEach(player => {
-           bulk.find({rosterId: player.rosterId}).upsert().replaceOne({...player})
-        });
-
-       await bulk.execute();
-       res.sendStatus(200);
-    });
+    await bulk.execute();
+    res.sendStatus(200);
+  });
 });
 
 app.listen(app.get('port'), () =>
-    console.log('Madden Data is running on port', app.get('port'))
+  console.log('Madden Data is running on port', app.get('port'))
 );
